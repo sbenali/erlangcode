dbd0ebb9dd469e3326d4cd01afb80edcecf55647
sbenali <samirbenali@live.com>
2016-12-08 20:21:58 +0000

    initial

---
+++ calendar_sb.erl
@@ -0,0 +1,117 @@
+-module(calendar_sb).
+
+-author("samir").
+
+-import(string,[substr/3, to_integer/1]).
+
+-export([get_month_info/2, get_month_days/4, get_utc_now/0, day_name/1, is_weekend/1, get_business_calendar/1,
+          get_public_holidays/0, is_public_holiday/2]).
+
+-define(MONTHS, [{1,"Jan","January",31},
+                 {2,"Feb","Feburary",0},{3,"Mar","March",31},{4,"Apr","April",30},
+                 {5,"May","May",31},{6,"Jun","June",30},{7,"Jul","July",31},
+                 {8,"Aug","August",31},{9,"Sept","September",30},{10,"Oct","October",31},
+                 {11,"Nov","November",30},{12,"Dec","December",31}]).
+%%FUNCTIONS
+
+day_name(D) when D > 0, D < 8 ->
+  case D of
+    1-> "Monday";
+    2-> "Tuesday";
+    3-> "Wednesday";
+    4-> "Thursday";
+    5-> "Friday";
+    6-> "Saturday";
+    7-> "Sunday"
+  end;
+day_name(_)->
+  invalid_day.
+
+get_month_info(M,Yr) ->
+  R = [{Mn,X,Y,Z} || {Mn,X,Y,Z}<-?MONTHS,Mn=:=M],
+  case R of
+    [] -> invalid_month;
+    _ ->
+      L = lists:last(R),
+      {Mnth,X,Y,_} = L,
+      if
+        Mnth =:= 2 ->
+          case calendar:is_leap_year(Yr) of
+            true -> {Mnth,X,Y,29};
+            false -> {Mnth,X,Y,28}
+          end;
+        true -> L
+      end
+  end.
+
+get_month_days(Mnth,Yr, IncludeWeekends, IncludePublicHolidays)->
+  Fdow = fun(X)->calendar:day_of_the_week(X) end,
+  Ftoutc = fun(X)->calendar:local_time_to_universal_time(X) end,
+  {_,_,_,DaysInMonth} = get_month_info(Mnth,Yr),
+  L = [{{Yr,Mnth,X},{0,0,0}} || X <- lists:seq(1,DaysInMonth)],
+  Utc = [Ftoutc(X)|| X<-L],
+  case IncludeWeekends of
+    true ->
+      case IncludePublicHolidays of
+        true ->
+          [{D,T,{dayofweek,Fdow(D),day_name(Fdow(D))}}|| {D,T}<-Utc];
+        false ->
+          Hols=get_public_holidays(),
+          [{D,T,{dayofweek,Fdow(D),day_name(Fdow(D))}}|| {D,T}<-Utc, is_public_holiday(D,Hols)=:=false]
+      end;
+    false ->
+      case IncludePublicHolidays of
+        true ->
+          [{D,T,{dayofweek,Fdow(D),day_name(Fdow(D))}}|| {D,T}<-Utc, is_weekend({D,T})=:=false];
+        false ->
+          L1 = [{D,T,{dayofweek,Fdow(D),day_name(Fdow(D))}}|| {D,T}<-Utc, is_weekend({D,T})=:=false],
+          Hols=get_public_holidays(),
+          [{R,R2,R3} || {R,R2,R3} <- L1, is_public_holiday({R,{0,0,0}},Hols)=:=false]
+      end
+  end.
+
+get_utc_now() ->
+  calendar:universal_time().
+
+is_public_holiday(D,H) ->
+  {DT,_}=D,
+  case calendar:valid_date(DT) of
+    true  ->
+      lists:member(D,H);
+    _ ->
+      {bad_date}
+  end.
+
+is_weekend(D)->
+  case D of
+    {D1,_} ->
+      DoW = calendar:day_of_the_week(D1),
+      case DoW of
+        6 -> true;
+        7 -> true;
+        _ -> false
+      end;
+    _ ->
+      {unknown}
+  end.
+
+get_business_calendar(Yr)->
+  U = [get_month_days(X,Yr,false,false) || X <- lists:seq(1,12)],
+  U.
+
+get_public_holidays()->
+  inets:start(),
+  ssl:start(),
+  {ok, {{_, 200, _}, _, Body}} = httpc:request(get, {"https://www.gov.uk/bank-holidays/england-and-wales.ics", []}, [], []),
+  inets:stop(),
+  ssl:stop(),
+  Lines = string:tokens(Body, "\n\r"),
+  Starts = [string:substr(L,20,8) || L <- Lines, string:substr(L,1,7)=:="DTSTART"],
+  Ends = [string:substr(L,18,8) || L <- Lines, string:substr(L,1,5)=:="DTEND"],
+  All = lists:merge(Starts,Ends),
+  Res = lists:map(fun(X) -> {{cln(substr(X,1,4)), cln(substr(X,5,2)), cln(substr(X,7,8))},{0,0,0}} end, All),
+  Res.
+
+cln(C)->
+  {V,_} = to_integer(C),
+  V.
---
+++ rnd_server.erl
@@ -0,0 +1,156 @@
+%%%-------------------------------------------------------------------
+%%% @author samir
+%%% @copyright (C) 2016
+%%% @doc
+%%%
+%%% @end
+%%% Created : 02. Dec 2016 19:01
+%%%-------------------------------------------------------------------
+-module(rnd_server).
+-author("samir").
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0,
+  generate_first_names/1,
+  generate_last_names/1,
+  generate_full_names/1,
+  generate_business_dates/1, generate_person/1]).
+
+%% gen_server callbacks
+-export([init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+generate_first_names(Max) ->
+  gen_server:call(?MODULE, {generate_fn,Max}).
+
+generate_last_names(Max) ->
+  gen_server:call(?MODULE, {generate_ln,Max}).
+
+generate_full_names(Max)->
+  gen_server:call(?MODULE, {generate_full, Max}).
+
+generate_business_dates(Yr)->
+  gen_server:call(?MODULE, {generate_dt, Yr}).
+
+generate_person(Max)->
+  gen_server:call(?MODULE, {generate_person, Max}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+init([]) ->
+  %% read in first names
+  read_and_save_csv("c:/samples/first_names.csv","FN"),
+  read_and_save_csv("c:/samples/last_names.csv","LN"),
+  read_and_save_csv("c:/samples/Towns_List.csv","TN"),
+  {ok, #state{}}.
+
+handle_call({generate_fn, N}, _From, State )->
+  {reply, gen_first_names(N), State};
+handle_call({generate_ln,N},_From, State)->
+  {reply, gen_last_names(N), State};
+handle_call({generate_full,N},_From, State)->
+  {reply, gen_full(N), State};
+handle_call({generate_dt,N},_From, State)->
+  {reply, calendar_sb:get_business_calendar(N), State};
+handle_call({generate_person,N},_From, State)->
+  {reply,gen_person(N),State};
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
+handle_info(_Info, State) ->
+  {noreply, State}.
+terminate(_Reason, _State) ->
+  ok.
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+gen_person(M)->
+  Towns = get("TN"),
+  Tl = erlang:length(Towns),
+  io:format("No. towns ~p~n",[Tl]),
+  People = gen_full(M),
+  Persons = [#{fullname => string:join([Y,X], ", "), age => random:uniform(99), town => lists:nth(random:uniform(Tl),Towns)}||{X,Y}<-People],
+  %%sort by fullname, if use sort/1 then it will sort by age
+  SortedPersons = lists:sort(fun(M1, M2)-> maps:get(fullname,M1) < maps:get(fullname,M2)  end, Persons),
+  SortedPersons.
+
+gen_full(M)->
+  Fn=gen_first_names(M),
+  Ln=gen_last_names(M),
+  lists:zip(Fn,Ln).
+
+
+gen_first_names(Max)->
+  gen_fn([],Max).
+
+gen_last_names(Max)->
+  gen_ln([],Max).
+
+gen_ln(A,Max) when Max>0 ->
+  N = get_name("LN"),
+  [N | gen_fn(A,Max-1)];
+gen_ln(A,0)->
+  A.
+
+gen_fn(A,Max) when Max>0 ->
+  N = get_name("FN"),
+  [N | gen_fn(A,Max-1)];
+gen_fn(A,0)->
+  A.
+
+
+get_name(K)->
+  Lfn=get(K),
+  lists:nth(rand:uniform(length(Lfn)), Lfn).
+
+read_and_save_csv(P,K)->
+  case get(K) of
+    undefined ->
+      case file:open(P,read) of
+        {ok,Device} ->
+          {ok,V} = file:read_line(Device),
+          L = string:tokens(V,"\r"),
+          if
+            length(L) =:= 1 ->
+              io:format("*** Found file with carriage return line feed ***~n"),
+              {ok,V2}=file:read_file(P),
+              L2 = string:tokens(erlang:binary_to_list(V2),"\r\n"),
+              put(K,L2);
+            true ->
+              io:format("*** Found file with carriage return only ***~n"),
+              put(K, L)
+          end;
+        _ ->
+          io:format("File ~p not found.",[P])
+      end;
+    _ ->
+      ignore
+  end.
+
+
+
